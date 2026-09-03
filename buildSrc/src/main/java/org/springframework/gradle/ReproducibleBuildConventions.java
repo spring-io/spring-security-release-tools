72b68b2ba6c5bf572823b7b29e29ea54686c9ed3
Josh Cummings <3627351+jzheaux@users.noreply.github.com>
2026-09-03 15:24:43 -0600

    Support Reproducible Builds
    Closes gh-104
    
    Signed-off-by: Josh Cummings <3627351+jzheaux@users.noreply.github.com>

---
+++ buildSrc/src/main/java/org/springframework/gradle/ReproducibleBuildConventions.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2002-present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.gradle;
+
+import org.gradle.api.Project;
+import org.gradle.api.tasks.bundling.AbstractArchiveTask;
+
+public class ReproducibleBuildConventions {
+
+	/**
+	 * Ensures that the archive always has the same time and file ordering between builds.
+	 *
+	 * @param project the current project
+	 */
+	public void apply(Project project) {
+		project.getTasks().withType(AbstractArchiveTask.class).configureEach(task -> {
+			task.setPreserveFileTimestamps(false);
+			task.setReproducibleFileOrder(true);
+		});
+	}
+
+}
