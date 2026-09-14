2d07445f0c9281fc7536c8909efd67b7c4570a5e
Josh Cummings <3627351+jzheaux@users.noreply.github.com>
2026-09-14 17:19:43 -0600

    Add Maven Pom Conventions
    Closes gh-109
    
    Signed-off-by: Josh Cummings <3627351+jzheaux@users.noreply.github.com>

---
+++ maven-plugin/src/main/java/io/spring/gradle/plugin/maven/ApacheLicense.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2002-2026 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.spring.gradle.plugin.maven;
+
+/**
+ * The Apache License, Version 2.0's name and URL.
+ *
+ * @author Josh Cummings
+ * @see SpringMavenPublishPlugin
+ * @see CheckMavenPomLicenseTask
+ */
+final class ApacheLicense {
+
+	static final String NAME = "Apache License, Version 2.0";
+
+	static final String URL = "https://www.apache.org/licenses/LICENSE-2.0";
+
+	private ApacheLicense() {
+	}
+
+}
